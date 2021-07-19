91a2e198f0f97cdd59a43adc0509c14b576b9e08
sergiofv55@gmail.com <sergio@DESKTOP-FHAKHB4.localdomain>
2021-07-18 20:41:55 -0600

    Action sheet

--- ejemplo/src/app/pages/list/list.page.ts
+++ ejemplo/src/app/pages/list/list.page.ts
@@ -4,6 +4,7 @@ import { ModalController } from '@ionic/angular';
 import { ViewNoteComponent } from 'src/app/components/view-note/view-note.component';
 import { NotesService } from 'src/app/services/notes.service';
 import { Note } from 'src/app/types/models';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -16,7 +17,8 @@ export class ListPage implements OnInit {
   constructor(
     private noteService: NotesService,
     public modalController: ModalController,
-    private router: Router) {}
+    private router: Router,
+    private actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
     //Sacamos las notas del servicio de notas
@@ -42,5 +44,49 @@ export class ListPage implements OnInit {
 
   deleteNote(nodeId: number){
     this.noteService.deleteNote(nodeId);
+  }
+
+  async presentActionSheet(nodeId: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: this.notes[nodeId-1].title,
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Borrar',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.deleteNote(nodeId);
+        }
+      }, {
+        text: 'Otros',
+        icon: 'gift',
+        handler: () => {
+          console.log('Others clicked');
+        }
+      }, {
+        text: 'Editar',
+        icon: 'chatbubble-ellipses-outline',
+        handler: () => {
+          this.goToEdit(nodeId);
+        }
+      }, {
+        text: 'Ver nota',
+        icon: 'chevron-up-circle-outline',
+        handler: () => {
+          this.viewNote(nodeId);
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 }
